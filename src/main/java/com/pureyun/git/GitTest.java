55e25d6d1cd2c5e4082293c4325a6e1472c03c63
pureyun <javac@88.com>
2024-05-23 14:10:06 +0800

    first commit

---
+++ src/main/java/com/pureyun/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.pureyun.git;
+
+/**
+ * @author: pureyun
+ * @Desc:
+ * @create: 2024-05-23 14:06
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
